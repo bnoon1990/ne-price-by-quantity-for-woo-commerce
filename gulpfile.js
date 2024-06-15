5d88d302e175884e6eecf3c05caf1bbd803cdfb0
Ben Noon <Ben@Nooneliterecruitment.com>
2024-06-15 22:48:32 +0100

    Updated gulpfile to create 2 seperate minified css packages for admin and frontend

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,28 @@ const gulp = require("gulp");
 const cleanCSS = require("gulp-clean-css");
 const concat = require("gulp-concat");
 
-gulp.task("minify-css", function () {
+gulp.task("minify-css-admin", function () {
   return gulp
-    .src("styles/**/*.css")
-    .pipe(concat("styles.css"))
+    .src("styles/admin/**/*.css")
+    .pipe(concat("admin.css"))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("minify-css-frontend", function () {
+  return gulp
+    .src("styles/frontend/**/*.css")
+    .pipe(concat("frontend.css"))
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("watch", function () {
-  gulp.watch("styles/**/*.css", gulp.series("minify-css"));
+  gulp.watch("styles/admin/**/*.css", gulp.series("minify-css-admin"));
+  gulp.watch("styles/frontend/**/*.css", gulp.series("minify-css-frontend"));
 });
 
-gulp.task("default", gulp.series("minify-css", "watch"));
+gulp.task(
+  "default",
+  gulp.series("minify-css-admin", "minify-css-frontend", "watch")
+);
