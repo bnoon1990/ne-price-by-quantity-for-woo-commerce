bf185a3045391844c7624a8a397fdee68af19b7b
Ben Noon <Ben@Nooneliterecruitment.com>
2024-06-15 23:44:49 +0100

    Wrapped main function in class for better organisation.

--- ne-price-by-quantity-for-woocommerce.php
+++ ne-price-by-quantity-for-woocommerce.php
@@ -1,57 +1,48 @@
 <?php
 
-/**
- * Plugin Name: Price by quantity for WooCommerce - By Noon Elite
- * Description: Adds variable pricing by quantity with options for percentage or fixed amount discounts and displays a pricing table on the product page.
- * Version: 1.0
- * Author: Ben Noon
- * License: GPL2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- */
-
-// Prevent direct access to the file
-if (!defined('ABSPATH')) {
-    exit;
-}
-
-// Enqueue admin scripts
-function bn_swo_qp_enqueue_admin_scripts($hook)
+class BN_Noon_Elite_Price_By_Quantity_For_Woocommerce_Plugin
 {
-    $screen = get_current_screen();
-    if ($screen->id === 'product') {
-        wp_enqueue_script('bn_swo_qp_admin_script', plugin_dir_url(__FILE__) . 'admin-script.js', array('jquery'), '1.11', true);
+    public function __construct()
+    {
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_public_scripts'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_public_styles'));
+
+        require_once plugin_dir_path(__FILE__) . 'admin/admin_settings.php';
+        require_once plugin_dir_path(__FILE__) . 'includes/product_pricing.php';
+        require_once plugin_dir_path(__FILE__) . 'frontend/pricing_table.php';
+    }
+
+    public function enqueue_admin_scripts($hook)
+    {
+        $screen = get_current_screen();
+        if ($screen->id === 'product') {
+            wp_enqueue_script('bn_swo_qp_admin_script', plugin_dir_url(__FILE__) . 'admin-script.js', array('jquery'), '1.11', true);
+        }
+    }
+
+    public function enqueue_public_scripts()
+    {
+        if (is_product()) {
+            wp_enqueue_script('bn_swo_qp_public_script', plugin_dir_url(__FILE__) . 'public-script.js', array('jquery'), '1.0', true);
+        }
+    }
+
+    public function enqueue_admin_styles($hook)
+    {
+        $screen = get_current_screen();
+        if ($screen->id === 'product') {
+            wp_enqueue_style('bn_swo_qp_admin_style', plugin_dir_url(__FILE__) . '/dist/admin.min.css', array(), '1.0');
+        }
+    }
+
+    public function enqueue_public_styles()
+    {
+        if (is_product()) {
+            wp_enqueue_style('bn_swo_qp_public_style', plugin_dir_url(__FILE__) . '/dist/frontend.min.css', array(), '1.0');
+        }
     }
 }
-add_action('admin_enqueue_scripts', 'bn_swo_qp_enqueue_admin_scripts');
 
-// Enqueue frontend scripts
-function bn_swo_qp_enqueue_public_scripts()
-{
-    if (is_product()) {
-        wp_enqueue_script('bn_swo_qp_public_script', plugin_dir_url(__FILE__) . 'public-script.js', array('jquery'), '1.0', true);
-    }
-}
-add_action('wp_enqueue_scripts', 'bn_swo_qp_enqueue_public_scripts');
-
-// Enqueue admin styles
-function bn_swo_qp_enqueue_admin_styles($hook)
-{
-    $screen = get_current_screen();
-    if ($screen->id === 'product') {
-        wp_enqueue_style('bn_swo_qp_admin_style', plugin_dir_url(__FILE__) . '/dist/admin.min.css', array(), '1.0');
-    }
-}
-add_action('admin_enqueue_scripts', 'bn_swo_qp_enqueue_admin_styles');
-
-// Enqueue public styles
-function bn_swo_qp_enqueue_public_styles()
-{
-    if (is_product()) {
-        wp_enqueue_style('bn_swo_qp_public_style', plugin_dir_url(__FILE__) . '/dist/frontend.min.css', array(), '1.0');
-    }
-}
-add_action('wp_enqueue_scripts', 'bn_swo_qp_enqueue_public_styles');
-
-require_once plugin_dir_path(__FILE__) . 'admin/admin_settings.php';
-require_once plugin_dir_path(__FILE__) . 'includes/product_pricing.php';
-require_once plugin_dir_path(__FILE__) . 'frontend/pricing_table.php';
+new BN_Noon_Elite_Price_By_Quantity_For_Woocommerce_Plugin();
