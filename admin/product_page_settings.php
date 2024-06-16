aea2fef3be0bcdf51b61af143ed42020c6a9f6a3
Ben Noon <Ben@Nooneliterecruitment.com>
2024-06-16 13:51:38 +0100

    Refactored product page settings to use class

--- admin/product_page_settings.php
+++ admin/product_page_settings.php
@@ -1,57 +1,54 @@
 <?php
-
-// Add custom product data tab
-function bn_swo_qp_add_custom_product_data_tab($tabs)
+class BN_Noon_Elite_Quantity_Pricing_Product_page_Settings
 {
-    $tabs['quantity_pricing'] = array(
-        'label'    => __('Quantity Pricing', 'woocommerce'),
-        'target'   => 'quantity_pricing_options',
-        'class'    => array('show_if_simple', 'show_if_variable'),
-        'priority' => 21,
-    );
-    return $tabs;
-}
-add_filter('woocommerce_product_data_tabs', 'bn_swo_qp_add_custom_product_data_tab');
-
-// Add custom fields to the custom product tab
-function bn_swo_qp_custom_product_data_fields()
-{
-    global $post;
-?>
-    <div id="quantity_pricing_options" class="panel woocommerce_options_panel">
-        <div class="options_group">
-            <?php
-            $pricing_rules = get_post_meta($post->ID, '_bn_swo_qp_pricing_rules', true);
-            $pricing_rules = $pricing_rules ? json_decode($pricing_rules, true) : array();
-
-            // Include the pricing table template
-            include plugin_dir_path(__FILE__) . 'templates/admin_pricing_table.php';
-            ?>
-        </div>
-    </div>
-<?php
-}
-add_action('woocommerce_product_data_panels', 'bn_swo_qp_custom_product_data_fields');
-
-// Save the custom fields
-function bn_swo_qp_save_pricing_rules($post_id)
-{
-    if (isset($_POST['bn_swo_qp_pricing_rules'])) {
-        $pricing_rules = $_POST['bn_swo_qp_pricing_rules'];
-        update_post_meta($post_id, '_bn_swo_qp_pricing_rules', json_encode($pricing_rules));
+    public function __construct()
+    {
+        add_filter('woocommerce_product_data_tabs', array($this, 'add_custom_product_data_tab'));
+        add_action('woocommerce_product_data_panels', array($this, 'custom_product_data_fields'));
+        add_action('woocommerce_process_product_meta', array($this, 'save_pricing_rules'));
+        add_action('wp_footer', array($this, 'output_pricing_rules'));
     }
-}
-add_action('woocommerce_process_product_meta', 'bn_swo_qp_save_pricing_rules');
 
-// Output pricing rules as a JS variable
-function bn_swo_qp_output_pricing_rules()
-{
-    if (is_product()) {
+    public function add_custom_product_data_tab($tabs)
+    {
+        $tabs['quantity_pricing'] = array(
+            'label'    => __('Quantity Pricing', 'woocommerce'),
+            'target'   => 'quantity_pricing_options',
+            'class'    => array('show_if_simple', 'show_if_variable'),
+            'priority' => 21,
+        );
+        return $tabs;
+    }
+
+    public function custom_product_data_fields()
+    {
         global $post;
+        echo '<div id="quantity_pricing_options" class="panel woocommerce_options_panel">';
+        echo '<div class="options_group">';
         $pricing_rules = get_post_meta($post->ID, '_bn_swo_qp_pricing_rules', true);
         $pricing_rules = $pricing_rules ? json_decode($pricing_rules, true) : array();
-        echo '<script type="text/javascript">var bn_swo_pricing_rules = ' . json_encode($pricing_rules) . ';</script>';
+        include plugin_dir_path(__FILE__) . 'templates/admin_pricing_table.php';
+        echo '</div>';
+        echo '</div>';
+    }
+
+    public function save_pricing_rules($post_id)
+    {
+        if (isset($_POST['bn_swo_qp_pricing_rules'])) {
+            $pricing_rules = $_POST['bn_swo_qp_pricing_rules'];
+            update_post_meta($post_id, '_bn_swo_qp_pricing_rules', json_encode($pricing_rules));
+        }
+    }
+
+    public function output_pricing_rules()
+    {
+        if (is_product()) {
+            global $post;
+            $pricing_rules = get_post_meta($post->ID, '_bn_swo_qp_pricing_rules', true);
+            $pricing_rules = $pricing_rules ? json_decode($pricing_rules, true) : array();
+            echo '<script type="text/javascript">var bn_swo_pricing_rules = ' . json_encode($pricing_rules) . ';</script>';
+        }
     }
 }
-add_action('wp_footer', 'bn_swo_qp_output_pricing_rules');
-?>
+
+new BN_Noon_Elite_Quantity_Pricing_Product_page_Settings();
